7d0194c4dff264c9665f3c339e72d5fcafda0d13
Bubka <858858+Bubka@users.noreply.github.com>
2024-12-13 15:02:38 +0100

    Remove Help view

--- src/popup/router/index.js
+++ src/popup/router/index.js
@@ -7,7 +7,6 @@ const router = createRouter({
         { path: '/setup', name: 'setup', component: () => import('../views/Setup.vue'), meta: { middlewares: [] } },
         { path: '/purpose', name: 'purpose', component: () => import('../views/Purpose.vue'), meta: { middlewares: [] } },
 
-        { path: '/help', name: 'help', component: () => import('../views/Help.vue') },
         { path: '/error', name: 'genericError', component: () => import('../views/Error.vue'), meta: { middlewares: [], watchedByKicker: true } },
         { path: '/404', name: '404', component: () => import('../views/Error.vue'), meta: { watchedByKicker: true }, props: true },
         { path: '/:pathMatch(.*)*', name: 'notFound', component: () => import('../views/Error.vue'), meta: { watchedByKicker: true }, props: true },
