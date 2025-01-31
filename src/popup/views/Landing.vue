d22c5d4bb7d4586cfb5b33eb77290cd8e8d55802
Bubka <858858+Bubka@users.noreply.github.com>
2025-01-31 13:44:48 +0100

    Adjust layout and some css styles

--- src/popup/views/Landing.vue
+++ src/popup/views/Landing.vue
@@ -6,8 +6,8 @@
     <div>
         <h1 class="title has-text-grey-dark" v-html="$t('title.popup.twofauth')"></h1>
         <p class="block">{{ $t('message.bind_2fauth_to_use_extension') }}</p>
-        <p class="block mt-6 has-text-centered">
-            <RouterLink id="btnConfigureMe" class="button is-link is-rounded is-main" :to="{ name: 'setup' }">{{ $t('message.configure') }}</RouterLink>
+        <p class="block has-text-centered">
+            <RouterLink id="btnConfigureMe" class="mt-5 button is-link is-rounded is-main is-medium" :to="{ name: 'setup' }">{{ $t('message.configure') }}</RouterLink>
         </p>
     </div>
     <VueFooter>
