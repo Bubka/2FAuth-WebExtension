e846b2e4e7b6ded5484ff36db636452224b1b88d
Bubka <858858+Bubka@users.noreply.github.com>
2025-02-07 18:27:32 +0100

    Update manifest and build script

--- vite.config.js
+++ vite.config.js
@@ -6,22 +6,32 @@ import webExtension, { readJsonFile } from 'vite-plugin-web-extension'
 // import vueDevTools from 'vite-plugin-vue-devtools'
 import { defineViteConfig as define } from './define.config'
 
-function generateManifest() {
-    const manifest = readJsonFile('manifest.json')
-    const pkg = readJsonFile('package.json')
-    return {
-        name: pkg.name,
-        description: pkg.description,
-        version: pkg.version,
-        ...manifest,
-    };
-}
+// function generateManifest() {
+//     const manifest = readJsonFile('manifest.json')
+//     const pkg = readJsonFile('package.json')
+//     return {
+//         name: pkg.name,
+//         description: pkg.description,
+//         version: pkg.version,
+//         ...manifest,
+//     };
+// }
 
 // https://vitejs.dev/config/
 export default defineConfig({
     build: {
         emptyOutDir: true,
-        sourcemap: true,
+        sourcemap: false,
+        rollupOptions: {
+            output: {
+                sanitizeFileName: (name) => {
+                    // Sanitizes file names generated during the build process:
+                    return name
+                    .replace(/\s+/g, '-') // Replaces spaces with dashes.
+                    .replace(/[^a-zA-Z0-9_.-]/g, ''); // Removes all invalid characters.
+                },
+            },
+        },
         // rollupOptions: {
         //     input: {
         //         // popup: 'src/popup/index.html',
@@ -34,7 +44,26 @@ export default defineConfig({
         vue(),
         // vueDevTools(),
         webExtension({
-            manifest: generateManifest,
+            manifest: () => {
+                const pkg = readJsonFile('package.json')
+                const template = readJsonFile('manifest.json')
+
+                const manifest = {
+                    ...template,
+                    name: pkg.displayName,
+                    description: pkg.description,
+                    version: '0.0.0',
+                    version_name: pkg.version,
+                }
+
+                const [semVer, major, minor, patch] = pkg.version.match(/^(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)(?:-((?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\+([0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?$/) ?? [];
+                
+                if (semVer != null) {
+                    manifest.version = [major, minor, patch].join('.')
+                }
+                
+                return manifest
+            },
             watchFilePaths: ['package.json', 'manifest.json'],
             browser: process.env.TARGET || "chrome",
         }),
