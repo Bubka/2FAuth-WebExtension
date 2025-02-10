d468e597988de34a9f3f7c7258397668876ee556
Bubka <858858+Bubka@users.noreply.github.com>
2025-02-10 15:33:06 +0100

    Enable sourcemap & Remove inappropriate auto-imports

--- wxt.config.ts
+++ wxt.config.ts
@@ -27,6 +27,7 @@ export default defineConfig({
             enabled: 9,
         },
         addons: {
+            // vueTemplate automatically set to true by @wxt-dev/module-vue
             vueDirectives: true,
         },
         imports: [
@@ -34,14 +35,8 @@ export default defineConfig({
             { name: 'onMessage', from: 'webext-bridge/background' },
             { name: 'sendMessage', from: 'webext-bridge/popup' },
         ],
-        dirs: [
-            './src/stores/**/*',
-            './src/entrypoints/popup/composables/**/*',
-            './src/entrypoints/popup/components/**/*',
-            './src/entrypoints/popup/layouts/**/*',
-            './src/entrypoints/popup/services/**/*',
-            './src/entrypoints/popup/stores/**/*',
-        ],
+        // dirs: [
+        // ],
         presets: [
             // vue preset is automatically imported by @wxt-dev/module-vue
             'vue-router',
@@ -52,10 +47,10 @@ export default defineConfig({
         
     },
     vite: () => ({
-        // build: {
-        //     emptyOutDir: true,
-        //     sourcemap: false,
-        // },
+        build: {
+            // emptyOutDir: true,
+            sourcemap: 'development' === process.env.NODE_ENV,
+        },
         plugins: [
             // vue(),
         ],
